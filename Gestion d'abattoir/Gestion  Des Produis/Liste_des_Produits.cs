fe627abddf64929e083b0e4dcc0d45e523953223
Abdelmalek BEDROH <A-malek-bedroh@outlook.com>
2022-11-24 12:18:49 +0100

    valider

---
+++ Gestion d'abattoir/Gestion  Des Produis/Liste_des_Produits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_d_abattoir.Gestion__Des_Produis
+{
+    public partial class Liste_des_Produits : UserControl
+    {
+        public Liste_des_Produits()
+        {
+            InitializeComponent();
+        }
+    }
+}
